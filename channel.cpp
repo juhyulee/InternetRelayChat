77619a91d2d56c50823966ff0befccc485bcd09f
siwolee <siwolee@student.42seoul.kr>
2023-10-20 01:53:16 +0900

    channel.cpp ongoing

--- channel.cpp
+++ channel.cpp
@@ -1,212 +1,118 @@
 #include "server.hpp"
-#include <cstdlib>
-#include <vector>
+
+//-------------------------------------------------------------------------------->>
+//oxodox
+//-------------------------------------------------------------------------------->>
+void 	Channel::initChannel(){
+	this->_name = "default";
+	this->_password = "";
+	this->_operator = std::map<std::string, Client> ();
+	this->_topic = "";
+	this->_mode = std::set<char>();
+	this->_user_cnt = 0;
+	this->_user_limit = 3;
+	this->_user_list = std::map<int, Client>();
+	this->_user_invite_list = std::map<int, Client>();
+}
 
 Channel::Channel() {
-	usrcnt = 0;
-	usrlimits = 3;
-	channelmode = std::set<char>();
+	this->initChannel();
 }
 
-void Channel::adduser(int fd, Client client) {
-	usrcnt += 1;
-	if (usrcnt > usrlimits) {
-		return ; //유저수 초과 메세지 전송
-	}
-	if (usrcnt == 1) {
-		addchanneloperator(client);
-	}
-	usrlist[fd] = client;
-}
+Channel::Channel(std::string name) {
+	this->initChannel();
+	this->_name = name;
+};
 
-Client *Channel::search_user(std::string nickname) {
-	for (std::map<int, Client>::iterator iter = this->usrlist.begin(); iter != this->usrlist.end(); ++iter) {
-		if (iter->second.nickname == nickname)
-		return &iter->second;
-	}
-	return NULL;
-}
+Channel::Channel(std::string name, Client & new_user){
+	this->initChannel();
+	this->_name = name;
+	// this->_operator.insert(make_pair(new_user.getFd(), new_user.getNickname())); 
+	//getFd getNickname 아직 없음
+};
 
-void Channel::deleteuser(int fd) {//유저 usrlist에서 지우는 함수
+Channel::~Channel(){};
 
-	if (usrlist.find(fd) != usrlist.end()) {
-		usrlist.erase(usrlist.find(fd));
-		usrcnt--;
-	}
-	else {
-		//에러메세지 전송
-	}
-}
 
-void Channel::inviteuser(int fd, Client client) {//채널에 유저 초대하는 함수
-	adduser(fd,client);
-}
 
-const std::string& Channel::getchannelname() const {
-	return this->channelname;
-}
+//-------------------------------------------------------------------------------->>
+//getter
+//-------------------------------------------------------------------------------->>
+const std::string&			Channel::getChannelName() const{
+	return (this->_name);
+};
+const std::string&			Channel::getChannelPassword() const{
+	return (this->_password);
+};
+const std::string&			Channel::getChannelTopic() const{
+	return (this->_topic);
+};
+const std::set<char>&		Channel::getChannelMode() const{
+	return (this->_mode);
+};
+int 						Channel::getUserCnt() const{
+	return (this->_user_cnt);
+};
+int 						Channel::getUserLimit() const{
+	return (this->_user_limit);
+};
 
-const std::string& Channel::getchannelpassword() const {
-	return this->channelpassword;
-}
 
-const std::map<std::string, Client>& Channel::getchanneloperator() const {
-	return this->channeloperator;
-}
 
-const std::string& Channel::getchanneltopic() const {
-	return this->channeltopic;
-}
+//-------------------------------------------------------------------------------->>
+//setter
+//-------------------------------------------------------------------------------->>
+void 						Channel::setChannelName(std::string new_name){
+	this->_name = new_name;
+};
+void 						Channel::setChannelPassword(std::string new_password){
+	this->_password = new_password;
+};
+void 						Channel::deleteChannelPassword(){
+	this->_password = "";
+	// mode 변경해야 하는지?
+	// 서버에서 권한 확인할 것인지?
+};
+void  						Channel::setChannelTopic(std::string new_topic){
+	this->_topic = new_topic;
+};
+std::vector<std::string>	Channel::*setChannelMode(Server &server, std::vector<std::string> token){
+	//??
+};
+void 						Channel::setUserLimit(int new_limits){
+	this->_user_limit = new_limits;
+};
 
-const std::set<char>& Channel::getchannelmode() const {
-	return this->channelmode;
-}
 
-int	Channel::getusrcnt() const {
-	return this->usrcnt;
-}
 
-int	Channel::getusrlimits() const {
-	return this->usrlimits;
-}
-
-void Channel::setchannelname(std::string chanellname) {
-	this->channelname = chanellname;
-}
-
-void Channel::setchannelpassword(std::string password) {
-	this->channelpassword = password;
-}
-
-void Channel::deletechannelpassword() {
-	this->channelpassword.erase();
-}
-
-void Channel::addchanneloperator(Client newoperator) {
-	this->channeloperator.insert(make_pair(newoperator.getnickname(), newoperator));
-}
-
-void Channel::removechanneloperator(Client newoperator) {
-	this->channeloperator.erase(newoperator.getnickname());
-}
-
-void Channel::setchanneltopic(std::string newtopic) {
-	this->channeltopic = newtopic;
-}
-
-std::vector<std::string> *Channel::setchannelmode(Server &server, std::vector<std::string> token) {
-	int switch_mode = 0;
-	(void)server;///???
-	std::set<char> channelmode = this->getchannelmode();
-	std::vector<std::string> *mode_params = new std::vector<std::string>;
-	if (token[2][0] == '+') {
-		switch_mode = 0;
-	}
-	else if (token[2][0] == '-') {
-		switch_mode = 1;
-	}
-	//초대만 가능한 채널로 설정 (i)
-	//채널 오퍼레이터만 토픽을 설정할 수 있음 (t)
-	else if (token[2][1] == 'i' || token[2][1] == 't') {
-		if (switch_mode == 0) {
-			if (channelmode.find(token[2][1]) == channelmode.end()) {
-				channelmode.insert(token[2][1]);
-				mode_params->push_back(token[2]);
-			}
-			else {
-				return 0;
-			}
+//-------------------------------------------------------------------------------->>
+//Msg
+//-------------------------------------------------------------------------------->>
+void 						Channel::broadcastChannelMessage(int send_fd, std::string message){
+	for (std::map<int, Client>::iterator iter = this->_user_list.begin(); \
+		iter != this->_user_list.end(); iter++){
+			if (iter->first == send_fd)
+				continue ;
+			// sendmsg --> 서버 필요한지?
 		}
-		else if (switch_mode == 1) {
-			if (channelmode.find(token[2][1]) != channelmode.end()) {
-				channelmode.erase(token[2][1]);
-				mode_params->push_back(token[2]);
-			}
-			else {
-				return 0;
-			}
-		}
-	}
-	// 채널 패스워드 설정 (k)
-	// 채널 오퍼레이터 권한줌 (o)
-	// 채널 유저 수 제한 (l)
-	else if (token[2][1] == 'k' || token[2][1] == 'o' || token[2][1] == 'l') {
-		if (token.size() < 4) {
-			return 0;
-		}
-		if (switch_mode == 0) {
-			if (token[2][1] == 'o') {
-				std::map<std::string, Client> channeloperator = this->getchanneloperator();
-				if (channeloperator.find(token[3]) == channeloperator.end()) {
-					Client *newoperator = this->search_user(token[3]);
-					this->addchanneloperator(*newoperator);
-					mode_params->push_back(token[2]);
-					mode_params->push_back(token[3]);
-				}
-				else {
-					return 0;
-				}
-			}
-			else if (channelmode.find(token[2][1]) == channelmode.end()) {
-				channelmode.insert(token[2][1]);
-				// parameter 저장
-				if (token[2][1] == 'k') {
-					this->setchannelpassword(token[3]);
-				}
-				else if (token[2][1] == 'l') {
-					this->setusrlimits(std::atoi(token[3].c_str()));
-				}
-				// reply 목록(command, parameter) 저장
-				mode_params->push_back(token[2]);
-				mode_params->push_back(token[3]);
-			}
-			else {
-				return 0;
-			}
-		}
-		else if (switch_mode == 1) {
-			if (token[2][1] == 'o') {
-				std::map<std::string, Client> channeloperator = this->getchanneloperator();
-				if (channeloperator.find(token[3]) != channeloperator.end()) {
-					Client *newoperator = search_user(token[3]);
-					this->removechanneloperator(*newoperator);
-					mode_params->push_back(token[2]);
-					mode_params->push_back(token[3]);
-				}
-				else {
-					return 0;
-				}
-			}
-			else if (channelmode.find(token[2][1]) != channelmode.end()) {
-				channelmode.erase(token[2][1]);
-				// parameter 초기화
-				if (token[2][1] == 'k') {
-					deletechannelpassword();
-				}
-				else if (token[2][1] == 'l') {
-					setusrlimits(100);
-				}
-				// reply 목록(command) 저장
-				mode_params->push_back(token[2]);
-			}
-			else {
-				return 0;
-			}
-		}
-	}
-	else {
-		return 0;
-	}
-	return mode_params;
-	// RPL_CHANNELMODE
-	// for (std::map<int,Client>::iterator iter = this->usrlist.begin();
-	// iter != this->usrlist.end(); iter++) {
-		// send_msg(RPL_MODE(this->usrlist[fd].getPrefix(), token[1], mode_params.at(0), mode_params.at(1)), iter->first);
-	// }
-}
-//특정 유저한테 보낼 메세지 서버로 보냄
-//전체 유저한테 보낼 메세지 서버로 보냄
+};
 
-void Channel::setusrlimits(int new_limits) {
-	this->usrlimits = new_limits;
-}
+
+
+//-------------------------------------------------------------------------------->>
+//operator
+//-------------------------------------------------------------------------------->>
+Client *		 				Channel::searchChannelOperator(std::string nickname) const{
+	for (std::map<std::string, Client *>::iterator iter = this->_operator.begin(); \
+		iter != this->_operator.end(); iter++){
+			if (iter->first == nickname)
+				return (iter->second);
+		}
+	return (NULL);
+};
+void 						Channel::addChannelOperator(Client new_operator){
+
+};
+void  						Channel::deleteChannelOperator(Client operator){
+
+};
--- channel.hpp
+++ channel.hpp
@@ -3,47 +3,70 @@
 
 #include "client.hpp"
 #include <set>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <iostream>
 
 class Server;
 
 class Channel {
 	private :
-		std::string channelname; // 채널이름
-		std::string channelpassword; //채널비밀번호
-		std::map<std::string, Client> channeloperator; //채널오퍼레이터
-		std::string channeltopic; //채널토픽
-		std::set<char> channelmode; //채널모드
-		int usrcnt; //유저 인원수
-		int usrlimits; //최대 유저 인원수
+		std::string _name; // 채널이름
+		std::string _password; //채널비밀번호
+		std::map<std::string, Client> _operator; //채널오퍼레이터
+		std::string _topic; //채널토픽
+		std::set<char> _mode; //채널모드
+		int _user_cnt; //유저 인원수
+		int _user_limit; //최대 유저 인원수
+		std::map<int, Client> _user_list; //유저목록
+		std::map<int, Client> _user_invite_list; //초대된 유저목록
+
+		Channel(const Channel & copy);
+		const Channel & operator=(const Channel & assign);
+		void	initChannel();
+
 	public :
 		Channel();
+		Channel(std::string name);
+		Channel(std::string name, Client & new_user);
+		~Channel();
 
-		std::map<int, Client> usrlist; //유저목록
+		//getter
+		const std::string&			getChannelName() const;
+		const std::string&			getChannelPassword() const;
+		// const std::map<std::string, Client>& getChannelOperator() const; >> search로 변경, operator로 내림
+		const std::string&			getChannelTopic() const;
+		const std::set<char>&		getChannelMode() const;
+		int 						getUserCnt() const;
+		int 						getUserLimit() const;
 
-		const std::string& getchannelname() const;
-		const std::string& getchannelpassword() const;
-		const std::map<std::string, Client>& getchanneloperator() const;
-		const std::string& getchanneltopic() const;
-		const std::set<char>& getchannelmode() const;
-		int	getusrcnt() const;
-		int	getusrlimits() const;
-
-		void setchannelname(std::string chanellname);
-		void setchannelpassword(std::string password);
-		void deletechannelpassword();
-		void addchanneloperator(Client newoperator);
-		void removechanneloperator(Client newoperator);
-		void setchanneltopic(std::string newtopic);
-		std::vector<std::string> *setchannelmode(Server &server, std::vector<std::string> token);
-		void setusrlimits(int new_limits);
+		//setter
+		void 						setChannelName(std::string new_name);
+		void 						setChannelPassword(std::string new_password);
+		void 						deleteChannelPassword();
+		void  						setChannelTopic(std::string new_topic);
+		std::vector<std::string>	*setChannelMode(Server &server, std::vector<std::string> token);
+		void 						setUserLimit(int new_limits);
+		
+		//msg
+		void 						broadcastChannelMessage(int send_fd, std::string message);// 채널에 메세지 보내는 함수
+		// ^^^ send_fd == -1 자기자신 포함, 유저일 때는 비포함
 		//특정 유저한테 보낼 메세지 서버로 보냄
 		//전체 유저한테 보낼 메세지 서버로 보냄
 
-		void adduser(int fd, Client client); //유저 채널에 추가하는 함수
-		Client *search_user(std::string nickname);
-		void deleteuser(int fd);//유저 usrlist에서 지우는 함수
-		void inviteuser(int fd, Client client);//채널에 유저 초대하는 함수
-		void broadcastChannelMessage(int send_fd, std::string message);// 채널에 메세지 보내는 함수
+		//operator
+		Client 		 				*searchChannelOperator(std::string nickname) const;
+		void 						addChannelOperator(Client new_operator);
+		void  						deleteChannelOperator(Client operator);
+
+
+		//user
+		Client  					*searchChannelUser(std::string nickname); // 유저 목록에 있는지 확인
+		Client  					*searchChannelInvite(std::string nickname); // 초대되었는지 확인
+		void 						addChannelUser(int fd, Client client); //유저 채널에 추가하는 함수
+		void 						deleteChannelUser(int fd);//유저 usrlist에서 지우는 함수
+		void 						inviteChannelUser(int fd, Client client);//채널에 유저 초대하는 함수
 };
 
 #endif
