4571171bcbbee7669d200a7d0f6fb81ccd030adb
Juhyun Lee <juhyulee@c2r5s6.42seoul.kr>
2023-10-31 00:17:40 +0900

    kfjdk

--- main.cpp
+++ main.cpp
@@ -27,6 +27,13 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
+	if (atoi(argv[1]) == 0) {
+		std::cout << "did you really input 0? well.. I'll just look at it this time" << std::endl;
+	}
+	if (atoi(argv[1]) > 65535) {
+		std::cout << "but i can't watch this" << std::endl;
+		return 1;
+	}
 	server.serverInit(argc, argv);
 
 	return 0;
